a808f929330417c1733387acf852407584ab5054
lijoy.george <lijoy.george@impelsys.com>
2019-07-11 17:27:36 +0530

    Add shuffled array suport

--- player.js
+++ player.js
@@ -6,39 +6,72 @@
 		* Set default settings
 		*/
 
-		this.page = 1;
-		this.lastPage = 10;
+		this.page = 0;
+		this.canNavigate = true;
 		this.firstPage = 1;
 		this.replay = 'TRUE';
 		this.referenceDocument = document;
 		this.currentElement = null;
+		// this.currentTip = {
+		// 	title: 'DEFAULT TITLE',
+		// 	content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+
+		// };
 		this.guideSrc = null;
 		this.guideType = null;
-		this.guide = {
-			title: 'DEFAULT TITLE',
-			content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+		this.guide = [
+			{
+				title: 'DEFAULT TITLE',
+				content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
 
-		}
+			},
+			{
+				title: 'DEFAULT TITLE',
+				content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+
+			}
+		]
 		this.guideWindow = null;
 	};
 
 	GLS.prototype = {
-		init: function (settings = {}, guid, referenceDocument) {
+		init: function (settings = {}, guide, referenceDocument) {
 
 			/*
 			* Init methrd to initilaze GLS player.
 			* Override settings.
 			*/
 
-			this.replay = settings.replay || this.replay;
+			this.replaySettings = settings.replay || this.replaySettings;
 			this.page = settings.page || this.page;
 			this.referenceDocument = referenceDocument || this.referenceDocument;
+			this.guideSrc = guide;
 
 			this.navigator.apply(this, []);
 			this.element.apply(this, []);
 			this.errorHandler.apply(this, []);
 			this.player.apply(this, []);
 			this.ui.apply(this, []);
+			this.operations.apply(this, []);
+
+			this.initGuide = function (guide) {
+				this.guideType = typeof guide;
+				if (guide && this.guideType == 'object') {
+					this.guide = guide;
+					this.firstPage = this.getFirstPage();
+					this.page = this.firstPage;
+				} else if (guide && this.guideType == 'string') {
+					// get json from guideSrc
+				} else {
+					this.throwError({
+						type: 'invalid guid type ',
+						msg: this.guideType
+					});
+				}
+			};
+
+			this.initGuide(this.guideSrc);
+
 		},
 		navigator: function () {
 
@@ -47,52 +80,54 @@
 			*/
 
 			this.next = function () {
-				if (this.page < this.lastPage) {
-					this.page++;
+				if (this.canNavigate) {
+					this.page = this.getIndexwitId(this.guide[this.page].next);
 					this.goto();
 				} else {
-					this.throwError({ type: 'last-page', msg: 'You are already reached Last page ' + this.page });
+					this.throwError({
+						type: 'last-page',
+						msg: 'You are already reached Last page ' + this.page
+					});
 				}
 			};
 			this.previous = function () {
-				if (this.page > 1) {
+				if (this.page > this.firstPage) {
 					this.page--;
 					this.goto();
 				} else {
-					this.throwError({ type: 'first-page', msg: 'You are already on page ' + this.page });
+					this.throwError({
+						type: 'first-page',
+						msg: 'You are already on page ' + this.page
+					});
 				}
 			};
 			this.goto = function (page = null) {
 				this.page = page || this.page;
-				console.log('Got to page: ', this.page);
+				this.renderTip();
+				this.canNavigate = this.guide[this.page].next;
 			};
 			this.skip = function () {
 				console.log('Next page skipped current page : ', this.page);
 			};
+			this.getFirstPage = function () {
+				return this.getIndexwitId(this.getFirstId());
+			};
 		},
 		player: function () {
-			this.play = function (guide, guideWindow = false) {
+			this.play = function (guideWindow = false) {
 
 				/*
 				* Play guid
 				*/
-
+				// this.currentTip = this.guide[this.page];
+				// console.log(this.currentTip.content);
 				if (!guideWindow) {
 					this.createGuideWindow();
 				} else {
 					this.guideWindow = guideWindow;
 				}
 
-				this.guideType = typeof guide;
-				this.guideSrc = guide;
-
-				if (guide && this.guideType == 'object') {
-					this.guide = guide;
-				} else if (guide && this.guideType == 'string') {
-					// get json from guideSrc
-				} else {
-					this.throwError({ type: 'invalid guid type ', msg: this.guideType });
-				}
+				this.renderTip();
 				this.show();
 			};
 			this.stop = function () {
@@ -132,24 +167,43 @@
 					case "XPATH":
 						return this.byXpath(this.reference.identifier);
 					default:
-						this.throwError({ type: 'unsupported', msg: 'Not supported reference ' + identifier });
+						this.throwError({
+							type: 'unsupported',
+							msg: 'Not supported reference ' + identifier
+						});
 						break;
 				}
 			};
 
 			this.byId = function (id) {
-				this.currentElement = this.referenceDocument.querySelector(id) || this.throwError({ type: 'no-ref', msg: 'no reference found with Id ' + id });
+				this.currentElement = this.referenceDocument.querySelector(id) ||
+					this.throwError({
+						type: 'no-ref',
+						msg: 'no reference found with Id ' + id
+					});
 				return this.currentElement;
 			};
 			this.byClass = function (className) {
-				this.currentElement = this.referenceDocument.querySelector(className) || this.throwError({ type: 'no-ref', msg: 'no reference found with Class Name ' + className });
+				this.currentElement = this.referenceDocument.querySelector(className) ||
+					this.throwError({
+						type: 'no-ref',
+						msg: 'no reference found with Class Name ' + className
+					});
 				return this.currentElement;
 
 			};
 			this.byXpath = function (xpath) {
 				// '/html/body/div'
-				this.currentElement = this.referenceDocument.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue ||
-					this.throwError({ type: 'no-ref', msg: 'no reference found with Xpath ' + xpath });
+				this.currentElement = this.referenceDocument.evaluate(
+					xpath,
+					document,
+					null,
+					XPathResult.FIRST_ORDERED_NODE_TYPE,
+					null).singleNodeValue ||
+					this.throwError({
+						type: 'no-ref',
+						msg: 'no reference found with Xpath ' + xpath
+					});
 				return this.currentElement;
 
 			};
@@ -159,7 +213,13 @@
 			this.getReference = function (selector) {
 				this.reference = {
 					identifier: selector,
-					type: selector.includes(".") ? 'CLASS' : selector.includes("#") ? 'ID' : selector.includes(":") ? 'XPATH' : ''
+					type: selector.includes(".") ?
+						'CLASS' :
+						selector.includes("#") ?
+							'ID' :
+							selector.includes(":") ?
+								'XPATH' :
+								''
 				}
 				return this.reference;
 			};
@@ -173,8 +233,8 @@
 			this.markup =
 				`<div id="guide">
 						<div class="guide-close" onclick="GLSPlayer.hide()">X</div>
-					<div class="guide-tip">
-						${this.guide.content}
+					<div class="guide-tip" id="gls-tip">
+						${this.guide[this.page].content}
 					</div>
 					<div class="button-wrap">
 						<a class="button" onclick="GLSPlayer.previous()">Previous</a>
@@ -188,29 +248,19 @@
 					border-radius: 50px;
 					padding: 4px;
 				}
-				.guide-title{
-					text-align: center;
-					padding: 4px;
-					border-bottom: 1px solid bisque;
-				}
 				.guide-tip {
 					padding: 4px;
-					text-align: unset;
-					font-family: none;
 					font-style: italic;
 					overflow: auto;
-					display: block;
 					margin-top: 8px
 				}
 				.guide-close {
 						float: right;
 						margin-right: 20px;
 						cursor: pointer;
-						display:inline-block;
-				
 				}
 				.button-wrap{
-				text-align: right;
+					text-align: right;
 					padding: 2px;
 				}
 				
@@ -229,12 +279,11 @@
 				}
 				else {
 					this.defaultStyle.appendChild(this.referenceDocument.createTextNode(this.css));
-
 				}
 				this.referenceDocument.getElementsByTagName("head")[0].appendChild(this.defaultStyle);
-
 			};
 			this.createGuideWindow = function () {
+
 				this.addStyle();
 
 				this.guideWindow = this.referenceDocument.createElement('DIV');
@@ -247,6 +296,39 @@
 			this.show = function () {
 				this.guideWindow.style.visibility = 'visible';
 			};
+			this.renderTip = function () {
+				console.log(this.guide[this.page].content);
+				this.referenceDocument.getElementById('gls-tip').innerHTML = this.guide[this.page].content;
+			}
+		},
+		operations: function () {
+			this.getSortedNumberIds = function (tipIds) {
+				return tipIds.sort(this.sortIds);
+			};
+			this.sortIds = function (a, b) {
+				return a - b;
+			};
+			Array.prototype.getLastItem = function () {
+				return this[this.length - 1];
+			};
+			this.getFirstId = function () {
+
+				return (this.getSortedNumberIds(
+					this.guide.map(
+						function (tip) {
+							return tip.id;
+						})
+				))[0];
+			};
+			this.getIndexwitId = function (id) {
+				for (var index = 0; index < this.guide.length; index++) {
+					if (this.guide[index].id == id) {
+						return index;
+					} else if (index == this.guide.length) {
+						return -1;
+					}
+				}
+			};
 		}
 	};
 	window.GLSPlayer = new GLS();
@@ -257,4 +339,21 @@
 /*
 IMPLEMENTATION
 */
-GLSPlayer.init({ replay: 'FALSE' }, document);
+GLSPlayer.init({ replay: 'FALSE' }, [{
+	"id": "1",
+	"content": "tip on first div",
+	"selector": "#id_1",
+	"next": "2"
+},
+{
+	"id": "3",
+	"content": "tip on third div.",
+	"selector": "div:eq(2)",
+	"next": null,
+},
+{
+	"id": "2",
+	"content": "tip on second div",
+	"selector": ".myClass2",
+	"next": "3",
+}], document);
